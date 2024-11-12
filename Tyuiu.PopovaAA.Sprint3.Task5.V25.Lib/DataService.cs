7f1fa5eb14cce5ad97a33d9e09d246805d0b6aaa
Tyuiu.PopovaAA <Tyuiu.PopovaAA@outlook.com>
2024-11-12 15:43:41 +0500

    5

---
+++ Tyuiu.PopovaAA.Sprint3.Task5.V25.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.PopovaAA.Sprint3.Task5.V25.Lib
+{
+    public class DataService : ISprint3Task5V25
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double sum = 0;
+            for (int i = startValue1; i <= stopValue1; i++)
+            {
+                for (int j = startValue2; j <= stopValue2; j++)
+                {
+                    sum = sum + (Math.Pow(x, startValue2) + Math.Cos(startValue2));
+                }
+            }
+            return Math.Round(sum, 3);
+        }
+    }
+}
---
+++ Tyuiu.PopovaAA.Sprint3.Task5.V25/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.PopovaAA.Sprint3.Task5.V25.Lib;
+namespace Tyuiu.PopovaAA.Sprint3.Task5.V25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнила Попова А.А. | РППб-24-1";
+            Console.WriteLine("*****************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                 *");
+            Console.WriteLine("* Тема: Вложенные циклы                                                     *");
+            Console.WriteLine("* Задание #5                                                                *");
+            Console.WriteLine("* Вариант #25                                                               *");
+            Console.WriteLine("* Выполнила: Попова Анастасия Алексеевна | РППб-24-1                        *");
+            Console.WriteLine("*****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                  *");
+            Console.WriteLine("* Написать программу, которая вычисляет сумму сумм ряда по формуле, при х=2:*");
+            Console.WriteLine("*     3   10                                                                *");
+            Console.WriteLine("* S = E   E  (x^k + cos(k))                                                 *");
+            Console.WriteLine("*    i=1 k=1                                                                *");
+            Console.WriteLine("*                                                                           *");
+            Console.WriteLine("*****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                          *");
+            Console.WriteLine("*****************************************************************************");
+
+            int x = 2;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startValue2 = 1;
+            int stopValue2 = 10;
+
+            double res = ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2);
+
+            Console.WriteLine("Старт шага первой суммы ряда = " + startValue1);
+            Console.WriteLine("Конец шага первой суммы ряда = " + stopValue1);
+            Console.WriteLine("Старт шага второй суммы ряда = " + startValue2);
+            Console.WriteLine("Конец шага Второй суммы ряда = " + startValue2);
+
+            Console.WriteLine("*****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                *");
+            Console.WriteLine("*****************************************************************************");
+
+            Console.WriteLine("Сумма сумм ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+            Console.ReadKey();
+        }
+    }
+}
